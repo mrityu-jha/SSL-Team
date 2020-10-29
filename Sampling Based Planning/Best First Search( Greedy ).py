5de79bbbe34976df4d474c7498039843af9ff45d
Mrityunjay Jha <mrityu.jha@gmail.com>
2020-10-29 05:36:44 +0530

    Best First Search Result added

--- Sampling Based Planning/Best First Search( Greedy ).py
+++ Sampling Based Planning/Best First Search( Greedy ).py
@@ -1,52 +1,42 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 27 19:21:38 2020
-
-@author: Mrityu
-
-Greedy Best-First grid planning
-"""
-import queue as queue
+from pprint import pprint
+from collections import deque
+import copy
+import grid_helper as gh
 
 
-def greedy_best_first_search(start_node, dest_node):
-    expanded_nodes = []  # to compare it with other algorithms
-    visited_nodes_queue = queue.PriorityQueue()  # to sort them base on their heuristic
-    maximum_memory_usage = 0  # to compare it with other algorithms
-    expanded_nodes.append(start_node)
-    start_node.visited = True
-    current_node = start_node
-    while True:
-        # check if we reach to dest or not
-        if dest_node.visited:
-            break
-        for x in current_node.children:
-            if not x.visited:
-                x.visited = True
-                x.parent = current_node
-                # x's cost from origin = parent's costFromOrigin from origin + distance between'em
-                x.costFromOrigin = current_node.costFromOrigin + current_node.children[x]
-                visited_nodes_queue.put((x.heuristic, x))
-        maximum_memory_usage = max(maximum_memory_usage, visited_nodes_queue.qsize())
-        current_node = visited_nodes_queue.get(0)[1]
-        expanded_nodes.append(current_node)
-    return expanded_nodes.__len__(), visited_nodes_queue.qsize(), maximum_memory_usage
+def scan_grid(grid, start=(0, 0)):
+    """Scan all grid, so we can find a path from 'start' to any point"""
+
+    q = deque()
+    q.append(start)
+    came_from = {start: None}
+    while len(q) > 0:
+        current_pos = q.popleft()
+        neighbors = gh.get_neighbors(grid, current_pos[0], current_pos[1])
+        for neighbor in neighbors:
+            if neighbor not in came_from:
+                q.append(neighbor)
+                came_from[neighbor] = current_pos
+
+    return came_from
 
 
-# won't check if they're visited or not
-def tree_greedy_best_first_search(start_node, dest_node):
-    expanded_nodes = []  # to compare it with other algorithms
-    visited_nodes_queue = queue.PriorityQueue()  # to sort them base on their heuristic to get next best node to visit
-    maximum_memory_usage = 0  # to compare it with other algorithms
-    expanded_nodes.append(start_node)
-    current_node = start_node
-    while True:
-        if expanded_nodes.__contains__(dest_node):
-            break
-        for x in current_node.children:
-            x.parent = current_node
-            visited_nodes_queue.put((x.heuristic, x))
-        maximum_memory_usage = max(maximum_memory_usage, visited_nodes_queue.qsize())
-        current_node = visited_nodes_queue.get(0)[1]
-        expanded_nodes.append(current_node)
-    return expanded_nodes.__len__(), visited_nodes_queue.qsize(), maximum_memory_usage
+def init():
+    initial_grid = gh.generate_grid_obstacle()
+    start_pos = (3, 0)
+    directions = scan_grid(initial_grid, start_pos)
+
+    path1 = gh.find_path(start_pos, (8, 8), directions)
+    # need copy as we modify the grid
+    grid_with_path1 = gh.draw_path(path1, copy.deepcopy(initial_grid))
+    pprint(grid_with_path1)
+    print(f"steps: {len(path1)}")
+
+    path2 = gh.find_path(start_pos, (4, 7), directions)
+    grid_with_path2 = gh.draw_path(path2, copy.deepcopy(initial_grid))
+    pprint(grid_with_path2)
+    print(f"steps: {len(path2)}")
+
+
+if __name__ == "__main__":
+    init()
---
+++ Sampling Based Planning/grid_helper.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 29 05:30:39 2020
+
+@author: Mrityu
+"""
+
+"""
+S - start point
+E - end point
+x - visited
+@ - path
+# - obstacle
+1 - cost
+"""
+
+import math
+
+START_COL = "S"
+END_COL = "E"
+VISITED_COL = "x"
+OBSTACLE_COL = "#"
+PATH_COL = "@"
+
+
+def generate_grid_empty():
+    return [[".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."]]
+
+
+def generate_grid_obstacle():
+    return [[".", ".", "#", ".", ".", ".", ".", ".", "."],
+            [".", ".", "#", ".", "#", ".", ".", ".", "."],
+            [".", ".", ".", ".", "#", ".", ".", ".", "."],
+            [".", ".", "#", ".", "#", ".", ".", ".", "."],
+            [".", ".", "#", ".", ".", ".", ".", ".", "."],
+            [".", ".", "#", ".", "#", ".", ".", ".", "."],
+            [".", ".", "#", "#", "#", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."]]
+
+
+def generate_grid_obstacle_for_b_star():
+    """
+    worst obstacle for B*
+    """
+
+    return [[".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", "#", "#", "#", "#", "#", "#", "#", "."],
+            [".", ".", ".", ".", ".", ".", ".", "#", "."],
+            [".", ".", ".", ".", ".", ".", ".", "#", "."],
+            [".", ".", ".", ".", ".", ".", ".", "#", "."],
+            [".", ".", ".", ".", ".", ".", ".", "#", "."],
+            [".", ".", ".", ".", ".", ".", ".", "#", "."],
+            [".", "#", "#", "#", "#", "#", "#", "#", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."]]
+
+
+def generate_grid_weighted():
+    """
+    weighted grid
+    """
+
+    return [[".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", "#", "#", "#", "#", ".", "#", "#", "."],
+            [".", ".", "2", "1", "9", ".", ".", "#", "."],
+            [".", ".", "9", "2", "2", ".", ".", "#", "."],
+            [".", ".", "9", "2", "2", ".", ".", "#", "."],
+            [".", ".", "2", "9", "9", ".", ".", "#", "."],
+            [".", ".", "9", "9", "9", ".", ".", "#", "."],
+            [".", "#", "#", "#", "#", "#", "#", "#", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "."]]
+
+
+def heuristic_distance(pos, end_pos, type="e"):
+    """
+    m - manhattan
+    e - euclidean
+    """
+
+    dx = abs(pos[0] - end_pos[0])
+    dy = abs(pos[1] - end_pos[1])
+
+    if type == "m":
+        return dx + dy
+
+    return math.sqrt(dx * dx + dy * dy)
+
+
+def find_path(start, end, came_from):
+    """Find the shortest path from start to end point"""
+
+    path = [end]
+
+    current = end
+    while current != start:
+        current = came_from[current]
+        path.append(current)
+
+    # reverse to have Start -> Target
+    # just looks nicer
+    path.reverse()
+
+    return path
+
+
+def get_cost(grid, pos):
+    col_val = grid[pos[0]][pos[1]]
+    return int(col_val) if col_val.isdigit() else 1
+
+
+def get_neighbors(grid, row, col):
+    height = len(grid)
+    width = len(grid[0])
+
+    neighbors = [(row + 1, col), (row, col - 1), (row - 1, col), (row, col + 1)]
+
+    # make path nicer
+    if (row + col) % 2 == 0:
+        neighbors.reverse()
+
+    # check borders
+    neighbors = filter(lambda t: (0 <= t[0] < height and 0 <= t[1] < width), neighbors)
+    # check obstacles
+    neighbors = filter(lambda t: (grid[t[0]][t[1]] != OBSTACLE_COL), neighbors)
+
+    return neighbors
+
+
+def draw_path(path, grid):
+    for row, col in path:
+        grid[row][col] = PATH_COL
+
+    # draw start and end
+    start_pos = path[0]
+    end_pos = path[-1]
+    grid[start_pos[0]][start_pos[1]] = START_COL
+    grid[end_pos[0]][end_pos[1]] = END_COL
+
+    return grid
